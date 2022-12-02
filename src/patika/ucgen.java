a0cf18474d3b4290e2cc41353c6ee1721766c9be
fatma <fatma@fatma>
2022-12-03 02:23:43 +0300

    first commit

---
+++ src/patika/ucgen.java
@@ -0,0 +1,26 @@
+package patika;
+import java.util.Scanner;
+public class ucgen {
+
+	public static void main(String[] args) {
+		double kenar1,kenar2,kenar3,alan,cevre,u;
+		Scanner input=new Scanner(System.in);
+		
+		System.out.println("lütfen üçgenin 1.kenarını giriniz:");
+        kenar1=input.nextDouble();
+        System.out.println("lütfen üçgenin 2.kenarını giriniz:");
+        kenar2=input.nextDouble();
+        System.out.println("lütfen üçgenin 3.kenarını giriniz:");
+        kenar3=input.nextDouble();
+     
+	    u=(kenar1+kenar2+kenar3)/2;
+	    cevre=2*u;
+	    alan=Math.sqrt(u*(u-kenar1)*(u-kenar2)*(u-kenar1));
+        System.out.println("üçgenin alanı:"+alan);
+		System.out.println("üçgenin çevresi:"+cevre);
+		
+		
+		
+	}
+
+}
